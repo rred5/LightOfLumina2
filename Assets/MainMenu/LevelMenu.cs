0d76e4ae7fb857addf69ad76df4236c14a756c5c
rred5 <66184102+rred5@users.noreply.github.com>
2024-06-23 15:15:23 -0500

    Re-added main menu
    Main Menu, level select, quit, volume control

---
+++ Assets/MainMenu/LevelMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OpenLevel(int levelId) {
+        string levelName = "Level " + levelId;
+        SceneManager.LoadScene(levelName);
+    }
+}
---
+++ Assets/MainMenu/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void quitGame()
+    {
+#if UNITY_STANDALONE
+        Application.Quit();
+#endif
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+}
